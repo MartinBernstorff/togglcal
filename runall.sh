58c0a7d25115a70237f35ebe7642a9da41cc0343
ryqiem <martinbernstorff@gmail.com>
2016-08-30 18:27:31 +0200

    Modified update interval to 30 mins

--- runall.sh
+++ runall.sh
@@ -2,6 +2,6 @@ bash togglcal.sh
 bash filter.sh
 bash reformat.sh
 bash gcal.sh
-echo "Finished all scripts. Sleeping for 300s"
-sleep 300
+echo "Finished all scripts. Sleeping for 30 mins"
+sleep 1800
 bash runall.sh
